671aa1a4a5c586b396775f4aa144675e3164857c
sabarinathjv <sabarinathjv1@gmail.com>
2021-10-02 22:49:51 +0530

    serializers added

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# admin.site.register(Article)
+
+
+
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')#to add  filter on right
+    list_display = ('title', 'description')#to add list view with data 
